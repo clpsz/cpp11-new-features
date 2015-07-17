36bed564f43e92ae9752ed8f9180770a36262636
clpsz <pengclpsz@gmail.com>
2015-07-17 10:38:16 +0800

    Add future example

--- multithread/main.cpp
+++ multithread/main.cpp
@@ -1,5 +1,18 @@
 #include "../common.h"
 #include <thread>
+#include <future>
+
+int heavyCalc()
+{
+    int count = 10000;
+    while (count--)
+    {
+    }
+
+    cout << "heavyCalc() finished" << endl;
+
+    return 33;
+}
 
 
 void task(int num)
@@ -15,8 +28,14 @@ void task(int num)
 
 int main(int argc, char *argv[])
 {
+    std::future<int> res = std::async(heavyCalc);
+
     std::thread t(task, 3);
     t.join();
+
+    // get should block heavyCalc finished
+    cout << res.get() << endl;
+
 
     return 0;
 }
